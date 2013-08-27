3dc12b7e079f63d586ddfdad7aca9c6bb3c9ee19
Guillaume Gardey <glinmac@gmail.com>
2013-08-27 09:09:03 +0100

    Fix error highlighting at the correct place.

--- run_pig.py
+++ run_pig.py
@@ -5,7 +5,6 @@ import os
 
 class RunPigCommand(sublime_plugin.WindowCommand):
     settings = None
-    error_regex = '^.*\<file (...*), line (\d+), column (\d+)\>.*$'
 
     def get_setting(self, name):
         setting = self.settings.get(name)
@@ -51,4 +50,4 @@ class RunPigCommand(sublime_plugin.WindowCommand):
         cmd.append(self.window.active_view().file_name())
 
         self.window.run_command('exec',
-            {'cmd': cmd, 'env': env, 'file_regex': self.error_regex, 'path': path})
+            {'cmd': cmd, 'env': env, 'file_regex': file_regex, 'path': path})
